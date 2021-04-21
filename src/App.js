c57a1baa31166296db1f0369477e393d362b6d91
jmendozacr <mendoza077@gmail.com>
2021-04-20 20:24:00 -0600

    adding styles with styled component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import { Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import About from './pages/About';
 import Blog from './pages/Blog';
@@ -7,23 +8,28 @@ import Home from './pages/Home';
 
 function App() {
     return (
-        <BrowserRouter>
-            <div>
-                <Header/>
-                <main>
-                    <Route exact path="/">
-                        <Home/>
-                    </Route>
-                    <Route path="/blog">
-                        <Blog/>
-                    </Route>
-                    <Route path="/about">
-                        <About/>
-                    </Route>
-                </main>
-            </div>
-        </BrowserRouter>
+        <Container>
+            <Header/>
+            <Main>
+                <Route exact path="/" component={Home} />
+                <Route path="/blog" component={Blog} />
+                <Route path="/about" component={About} />
+            </Main>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    padding: 40px;
+    width: 90%;
+    max-width: 700px;
+`;
+
+const Main = styled.main`
+    background: #fff;
+    padding: 40px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
 
 export default App;
