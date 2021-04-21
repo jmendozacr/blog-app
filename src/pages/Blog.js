d5b76b966f9e7dd48f4ddc80c0193625c7bfda30
jmendozacr <mendoza077@gmail.com>
2021-04-20 19:02:12 -0600

    adding react-router and style-component, adding page to create routes

---
+++ src/pages/Blog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h2>Blog Page</h2>
+            <li>Article #1</li>
+            <li>Article #2</li>
+            <li>Article #3</li>
+        </div>
+    );
+}
+
+export default Blog;
